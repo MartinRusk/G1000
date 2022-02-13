ae32be30355106227d85181cbe3ab499aa17539b
MartinRusk <martin@ruskowski.de>
2022-02-13 23:02:02 +0100

    Enhanced XPlane config

--- config/XPlane/mrusk_panel.lua
+++ config/XPlane/mrusk_panel.lua
@@ -1,9 +1,11 @@
 -- adaption for DA62 panel
 
--- link datarefs
+-- create new datarefs
 flap_led = create_dataref_table("mrusk/panel/flap", "IntArray")
 gear_led = create_dataref_table("mrusk/panel/gear", "IntArray")
 deice_led = create_dataref_table("mrusk/panel/deice", "IntArray")
+define_shared_DataRef("parking_brake", "mrusk/panel/brake_sw")
+-- link used datarefs
 dataref("flap_ratio", "sim/flightmodel2/controls/flap1_deploy_ratio")
 gear_ratio = dataref_table("sim/flightmodel2/gear/deploy_ratio")
 dataref("gear_unsafe", "sim/cockpit/warnings/annunciators/gear_unsafe")
@@ -19,6 +21,7 @@ gear_led[3] = 0
 deice_led[0] = 0
 deice_led[1] = 0
 deice_led[2] = 0
+parking_brake = 0
 
 -- Deice only with DA62
 if (PLANE_ICAO == "DA62") then
@@ -71,6 +74,7 @@ function mrusk_panel_handle()
     deice_led[1] = deice_high
     deice_led[2] = deice_max
 
+    sim/flight_controls/brakes_max = mrusk/panel/brake_sw
 end
 
 do_every_draw("mrusk_panel_handle()")
