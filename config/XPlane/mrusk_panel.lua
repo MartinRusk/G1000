4a520351ca3fd059eafae0ccbb326c358c868528
MartinRusk <martin@ruskowski.de>
2022-03-14 15:23:39 +0100

    V1.5.0: X-Plane config update

--- config/XPlane/mrusk_panel.lua
+++ config/XPlane/mrusk_panel.lua
@@ -4,11 +4,20 @@
 flap_led = create_dataref_table("mrusk/panel/flap", "IntArray")
 gear_led = create_dataref_table("mrusk/panel/gear", "IntArray")
 deice_led = create_dataref_table("mrusk/panel/deice", "IntArray")
-define_shared_DataRef("parking_brake", "mrusk/panel/brake_sw")
+brake_sw = create_dataref_table("mrusk/panel/brake", "IntArray")
+
 -- link used datarefs
 dataref("flap_ratio", "sim/flightmodel2/controls/flap1_deploy_ratio")
 gear_ratio = dataref_table("sim/flightmodel2/gear/deploy_ratio")
 dataref("gear_unsafe", "sim/cockpit/warnings/annunciators/gear_unsafe")
+--dataref("parking_brake", "sim/flightmodel/controls/parkbrake")
+
+--sim/operation/flightmodel_speed_change 
+--sim/time/sim_speed	int	y	ratio	This is the multiplier for real-time...1 = realtime, 2 = 2x, 0 = paused, etc.
+--sim/time/sim_speed_actual	float	n	ratio	The actual time-speed increase the sim has achieved - takes into account fps limiting.
+--sim/time/sim_speed_actual_ogl	float	n	ratio	The actual time-speed increase the sim has achieved - takes into account fps limiting and time dilation if fps<19.
+--sim/time/ground_speed	int	y	ratio	This is the multiplier on ground speed, for faster travel via double-distance
+--sim/time/ground_speed_flt	float	y	ratio	This is the multiplier on ground speed, as float value, for faster travel via double-distance
 
 -- initialize tables
 flap_led[0] = 0
@@ -21,7 +30,7 @@ gear_led[3] = 0
 deice_led[0] = 0
 deice_led[1] = 0
 deice_led[2] = 0
-parking_brake = 0
+brake_sw[0] = 0
 
 -- Deice only with DA62
 if (PLANE_ICAO == "DA62") then
@@ -70,11 +79,26 @@ function mrusk_panel_handle()
     gear_led[3] = gear_unsafe
 
     -- deice
-    deice_led[0] = deice_norm
-    deice_led[1] = deice_high
-    deice_led[2] = deice_max
+    if deice_norm > 0 then
+        deice_led[0] = 1
+    else
+        deice_led[0] = 0
+    end
+    if deice_high > 0 then
+        deice_led[1] = 1
+    else
+        deice_led[1] = 0
+    end
+    if deice_max > 0 then
+        deice_led[2] = 1
+    else
+        deice_led[2] = 0
+    end
 
-    sim/flight_controls/brakes_max = mrusk/panel/brake_sw
+    -- brake
+    -- if brake_sw[0] then
+    --     command_once("sim/flight_controls/brakes_max")
+    -- end 
 end
 
 do_every_draw("mrusk_panel_handle()")
